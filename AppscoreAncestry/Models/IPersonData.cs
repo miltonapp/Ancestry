63196e1441ce58ed59b3e903d9b8cc747906b33f
mchen <milton.chen@msn.com>
2018-10-07 17:41:59 +1100

    Build controllers for task 2.

---
+++ AppscoreAncestry/Models/IPersonData.cs
@@ -0,0 +1,13 @@
+﻿namespace AppscoreAncestry.Models
+{
+    public interface IPersonData
+    {
+        int ID { get; }
+
+        string Name { get; }
+
+        string Gender { get; }
+
+        string BirthPlace { get; }
+    }
+}
---
+++ AppscoreAncestry/Models/PersonData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppscoreAncestry.Models
+{
+    public class PersonData : IPersonData
+    {
+        private Person _person { get; set; }
+
+        public PersonData(Person person)
+        {
+            if (person == null)
+            {
+                throw new NullReferenceException("person cannot be null");
+            }
+            _person = person;
+        }
+
+        public int ID { get => _person.ID; }
+
+        public string Name { get => _person.Name; }
+
+        public string Gender { get => _person.Gender; }
+
+        public string BirthPlace
+        {
+            get
+            {
+                string result = "";
+                if (_person.PlaceObj != null)
+                {
+                    result = _person.PlaceObj.Name;
+                }
+                return result;
+            }
+        }
+    }
+}
